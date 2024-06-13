c754b69641c74a1d1e2c5199ff39f4dabeaaf59a
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-06-13 17:50:04 +0530

    Add meals listview

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({
+    required this.title,
+    required this.meals,
+    super.key,
+  });
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, idx) => Text(meals[idx].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh...Nothing here!',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
