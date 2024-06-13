71a27aa91768079c5a0de539b6e706b5fdc2a35d
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-06-13 17:10:51 +0530

    Add catgories grid data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -16,7 +18,9 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: [],
+        children: [
+          for (final category in availableCategories) CategoryGridItem(category: category)
+        ],
       ),
     );
   }
