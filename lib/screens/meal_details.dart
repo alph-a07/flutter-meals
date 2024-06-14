581276e5129c8f88397a5e13fe2e776c2916cdd8
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-06-14 13:38:44 +0530

    Add meal details screen

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealDetails extends StatelessWidget {
+  const MealDetails({required this.meal, super.key});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
