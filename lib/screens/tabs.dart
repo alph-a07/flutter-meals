4e261e1455ce76cfb2a11f41af7bd282bcc4dd48
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-06-14 15:08:09 +0530

    Add snackbar on favorites toggle

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -14,14 +14,26 @@ class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoritesMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     if (_favoritesMeals.contains(meal)) {
       setState(() {
         _favoritesMeals.remove(meal);
+        _showInfoMessage('Removed from favorites');
       });
     } else {
       setState(() {
         _favoritesMeals.add(meal);
+        _showInfoMessage('Added to favorites');
       });
     }
   }
